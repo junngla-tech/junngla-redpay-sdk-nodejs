b13d515263cf8d26c7100000228d2224c1dd4d5c
Camilo <camilo.mh2014@gmail.com>
2024-12-30 18:08:11 -0300

    feat: importaciones de modulos

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,48 @@
-export * from './Integrity.service';
+/**
+ * Exporta todos los módulos principales de la biblioteca para facilitar su uso.
+ */
+
+import "reflect-metadata";
+
+/**
+ * Funciones relacionadas con la generación y validación de firmas para garantizar la integridad de los datos.
+ * @module Integrity.service
+ */
+export * from "./Integrity.service";
+
+/**
+ * Enumeraciones que definen valores clave y constantes utilizadas en diferentes partes de la biblioteca.
+ * @module enum
+ */
+export * from "./enum";
+
+/**
+ * Interfaces que definen las estructuras de datos y contratos utilizados en los servicios y modelos.
+ * @module interface
+ */
+export * from "./interface";
+
+/**
+ * Clases y abstracciones principales utilizadas para manejar entidades como usuarios, tokens y fillers.
+ * @module model
+ */
+export * from "./model";
+
+/**
+ * Proveedor de configuración para la inicialización y uso global de la biblioteca.
+ * @module provider
+ */
+export * from "./provider";
+
+/**
+ * Servicios principales que interactúan con los endpoints de RedPay.
+ * @module services
+ */
+export * from "./services";
+
+/**
+ * Tipos definidos para mejorar la claridad y seguridad de los datos utilizados en la biblioteca.
+ * @module types
+ */
+export * from "./types";
+
