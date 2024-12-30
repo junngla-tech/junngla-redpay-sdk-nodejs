8c6a7120b2c26bfde00f41c1e0e170fa16b66e7b
Camilo <camilo.mh2014@gmail.com>
2024-12-30 18:04:59 -0300

    feat: constantes de ambientes

---
+++ src/config/constants.ts
@@ -0,0 +1,3 @@
+export const API_URL_PRODUCTION = "https://api.redpay.cl";
+export const API_URL_INTEGRATION = "https://localhost:3010";
+// export const API_URL_INTEGRATION = "https://test.api.redpay.junngla.com";
