3dbc62b87950ec71b948afb12b782ce11385c334
Camilo <camilo.mh2014@gmail.com>
2025-01-13 16:04:31 -0300

    feat: url productiva e integración

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,3 +1,2 @@
 export const API_URL_PRODUCTION = "https://api.redpay.cl";
-export const API_URL_INTEGRATION = "https://localhost:3010";
-// export const API_URL_INTEGRATION = "https://test.api.redpay.junngla.com";
+export const API_URL_INTEGRATION = "https://test.api.redpay.junngla.com";
