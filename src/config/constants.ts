543a3e95a415fb3f693db6e925e709192d17c0b5
Camilo <camilo.mh2014@gmail.com>
2025-01-17 16:04:02 -0300

    feat: constantes de intento maximo y código de reintento

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,3 +1,5 @@
 export const API_URL_PRODUCTION = "https://api.redpay.cl";
 export const API_URL_INTEGRATION = "https://test.api.redpay.junngla.com";
 export const STATUS_CODE_OK = "00-000";
+export const STATUS_CODE_RETRY = "07-006";
+export const RETRY_LIMIT = 3;
