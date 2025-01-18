8eed3496ee1f98660f81bae7d8ab3a82054e4bef
Camilo <camilo.mh2014@gmail.com>
2025-01-18 02:26:27 -0300

    refactor: añade identificador unico de autorización

--- src/model/Order.ts
+++ src/model/Order.ts
@@ -9,6 +9,9 @@ export class Order extends ClassBase<Order> {
   user_id!: string;
 
   @Expose()
+  authorization_id!: string;
+
+  @Expose()
   @Transform(({ value }) => value ?? 1)
   reusability?: number;
 
